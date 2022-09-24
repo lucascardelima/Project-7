fa5515c18ed3a405d7cac4fc2dafa1b91bc78b62
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-09-24 21:19:44 +0100

    added createcomment ctrl into posts router

--- groupomania/backend/routes/posts.js
+++ groupomania/backend/routes/posts.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const postsCtrl = require('../controllers/posts');
+const commentsCtrl = require('../controllers/comments');
 
 router.post('/posts/createpost', postsCtrl.createPost);
+router.post('/posts/createcomment', commentsCtrl.createComment);
 
 
 module.exports = router;
