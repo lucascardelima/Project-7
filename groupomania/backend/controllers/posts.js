698aa0fad3f212a257844a7ac086a27f46fba51c
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-09-24 19:05:04 +0100

    create post controller

--- groupomania/backend/controllers/posts.js
+++ groupomania/backend/controllers/posts.js
@@ -2,7 +2,33 @@ const sql = require('mssql');
 const dbconfig = require('../config/dbconfig').config;
 const crypto = require('crypto');
 
-exports.createPost = async (req.res.next) => {
+exports.createPost = async (req, res, next) => {
     let pool = await sql.connect(dbconfig)
+    let request = new sql.Request(pool);
+    const postID = crypto.randomUUID();
+    const url = req.protocol + '://' + req.get('host');
+
+    request.input('postID', sql.NVarChar, userID)
+    .input('userID', sql.NVarChar, req.body.userID)
+    .input('postTitle', sql.NVarChar, req.body.postTitle)
+    .input('postCreationDate', sql.DateTime, new Date())
+    .input('postEditDate', sql.DateTime, new Date())
+    .input('postText', sql.NVarChar, req.body.postText)
+    .input('postCategory', sql.NVarChar, req.body.postCategory)
+    .input('imageUrl', sql.NVarChar, url + '/images/' + req.file.filename)
+    .execute('createPost').then(
+        () => {
+            res.status(200).json({
+                success: 'Post Created successfully'
+            })
+        }
+    ).catch (
+        (error) => {
+            res.status(500).json({
+                error: error
+            })
+            sql.close();
+        }
+    )
 }
 
