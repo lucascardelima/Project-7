990df131c81dd91f942415f914f13025bbbfb64b
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-10-04 19:52:47 +0100

    add delete and update routes for comments and post

--- groupomania/backend/routes/posts.js
+++ groupomania/backend/routes/posts.js
@@ -5,7 +5,14 @@ const postsCtrl = require('../controllers/posts');
 const commentsCtrl = require('../controllers/comments');
 
 router.post('/posts/createpost', postsCtrl.createPost);
+router.post('/posts/deletepost', postsCtrl.deletePost);
+router.post('/posts/updatepost', postsCtrl.updatePost);
 router.post('/posts/createcomment', commentsCtrl.createComment);
+router.post('/posts/updatecomment', commentsCtrl.updateComment);
+router.post('/posts/deletecomment', commentsCtrl.deleteComment);
+
+
+
 
 
 module.exports = router;
