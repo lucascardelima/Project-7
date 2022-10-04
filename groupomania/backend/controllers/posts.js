ae5fb274980e82e476f5bb21f01c441eddd74c9e
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-10-04 19:29:30 +0100

    delete post added

--- groupomania/backend/controllers/posts.js
+++ groupomania/backend/controllers/posts.js
@@ -33,3 +33,52 @@ exports.createPost = async (req, res, next) => {
     )
 }
 
+exports.updatePost = async (req, res, next) => {
+    let pool = await sql.connect(dbconfig);
+    let request = new sql.Request(pool);
+    let currentDate = new Date();
+
+    request.input('postText', sql.NVarChar, req.body.postText)
+    .input('postID', sql.NVarChar, req.body.postID)
+    .input('postTitle', sql.NVarChar, req.body.postID)
+    .input('postCategory', sql.NVarChar, req.body.postCategory)
+    .input('userID', sql.NVarChar, req.body.userID)
+    .input('postEditDate', sql.DateTime, currentDate)
+    .execute('updatePost').then(
+        () => {
+            res.status(200).json({
+                success: 'Comment updated successfully'
+            })
+       }
+    ).catch (
+        (error) => {
+            res.status(500).json({
+                error: error
+            })
+            sql.close();
+        }
+    )
+}
+
+exports.deletePost = async (req, res, next) => {
+    let pool = await sql.connect(dbconfig);
+    let request = new sql.Request(pool);
+    
+    request.input('postID', sql.NVarChar, req.body.postID)
+    .input('userID', sql.NVarChar, req.body.userID)
+    .execute('deletePost').then(
+        () => {
+            res.status(200).json({
+                success: 'Comment deleted successfully'
+            })
+        }
+    ).catch (
+        (error) => {
+            res.status(500).json({
+                error: error
+            })
+            sql.close();
+        }
+    )
+}
+
