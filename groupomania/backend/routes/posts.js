2228020d2820cee469ec76b4213657ea280a24aa
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-10-04 19:58:30 +0100

    removed the comments router from posts

--- groupomania/backend/routes/posts.js
+++ groupomania/backend/routes/posts.js
@@ -2,14 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const postsCtrl = require('../controllers/posts');
-const commentsCtrl = require('../controllers/comments');
 
-router.post('/posts/createpost', postsCtrl.createPost);
-router.post('/posts/deletepost', postsCtrl.deletePost);
-router.post('/posts/updatepost', postsCtrl.updatePost);
-router.post('/posts/createcomment', commentsCtrl.createComment);
-router.post('/posts/updatecomment', commentsCtrl.updateComment);
-router.post('/posts/deletecomment', commentsCtrl.deleteComment);
+router.post('/createpost', postsCtrl.createPost);
+router.post('/deletepost', postsCtrl.deletePost);
+router.post('/updatepost', postsCtrl.updatePost);
 
 
 
