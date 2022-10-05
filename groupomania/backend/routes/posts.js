7c85de7dee50a4760920d6ce10660870bbfbb614
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-10-05 20:57:26 +0100

    added the get path

--- groupomania/backend/routes/posts.js
+++ groupomania/backend/routes/posts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const postsCtrl = require('../controllers/posts');
 
+router.get('/getposts', postsCtrl.getPosts);
 router.post('/createpost', postsCtrl.createPost);
 router.post('/deletepost', postsCtrl.deletePost);
 router.post('/updatepost', postsCtrl.updatePost);
