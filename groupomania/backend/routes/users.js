7c4cefd844af608b4405c0916ba811a3136b0c90
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-10-05 21:33:13 +0100

    added route to get unique user

--- groupomania/backend/routes/users.js
+++ groupomania/backend/routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usersCtrl = require('../controllers/users');
 
 router.get('/users/getusers', usersCtrl.getUsers);
+router.get('/users/getuser', usersCtrl.getUser);
 router.post('/users/login', usersCtrl.login);
 router.post('/users/insertuser', usersCtrl.insertUsers);
 
