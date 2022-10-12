5fed07274759c23e6b6c075a2a10eeb437d9d4eb
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-10-12 16:08:21 +0100

    updateuser router implemented

--- groupomania/backend/routes/users.js
+++ groupomania/backend/routes/users.js
@@ -7,5 +7,6 @@ router.get('/getusers', usersCtrl.getUsers);
 router.get('/getuser', usersCtrl.getUser);
 router.post('/login', usersCtrl.login);
 router.post('/signup', usersCtrl.signup);
+router.post('/updateuser', usersCtrl.updateUser);
 
 module.exports = router;
