a748f675b442247516ba5d7882fe48361d816a2b
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-04 12:38:19 +0000

    created footbar

---
+++ groupomania/groupomania-frontend/src/components/FooterBar.vue
@@ -0,0 +1,22 @@
+<style scoped>
+  footer {
+    border-top: 1px solid #797D7F;
+  }
+</style>
+
+<scrypt setup>
+
+</scrypt>
+
+<template>
+  <div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+      <p class="col-md-4 mb-0 text-muted">&copy; 2022 Groupomania</p>
+
+      <ul class="nav col-md-4 justify-content-end">
+        <li class="nav-item"><router-link to="/" class="nav-link px-2 text-muted">Home</router-link></li>
+        <li class="nav-item"><router-link to="/createpost" class="nav-link px-2 text-muted">Create Post</router-link></li>
+      </ul>
+    </footer>
+  </div>
+</template>
