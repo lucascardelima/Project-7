c915d944f9412039da3c2316579df7d9af62a682
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-04 14:46:05 +0000

    implemente the posts http request

--- groupomania/groupomania-frontend/src/views/HomePage.vue
+++ groupomania/groupomania-frontend/src/views/HomePage.vue
@@ -2,7 +2,41 @@
 
 </style>
 
+<script>
+  import PostCard from '../components/PostCard.vue'
+  
+  export default {
+    name: 'HomePage',
+    data() {
+      return {
+
+      }
+    },
+    components: {
+      PostCard
+    },
+    methods: {
+      getPosts() {
+        axio.get('')
+      }
+    }
+  }
+</script>>
+
 <template>
-    <div class="container py-5"></div>
+
+<div class="container mt-4 mb-5">
+  <div class="d-flex justify-content-center row">
+    <div class="col-md-8">
+      <div class="feed p-2">
+           
+        <PostCard/>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+    
 </template>
 
