da3d03c07a4d33df07c5078856035265678ca5c8
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-04 12:40:42 +0000

    import axios

--- groupomania/groupomania-frontend/src/main.js
+++ groupomania/groupomania-frontend/src/main.js
@@ -3,10 +3,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 
 const app = createApp(App);
 app.use(router);
+app.use(VueAxios, axios);
 app.mount("#app");
+
 
 import "bootstrap/dist/js/bootstrap.js";
