479999f75557a42b6e4076d67a747e43f8d9a5d9
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-08 22:44:52 +0000

    re-style the navbar to be responsive

--- groupomania/groupomania-frontend/src/components/NavBar.vue
+++ groupomania/groupomania-frontend/src/components/NavBar.vue
@@ -5,11 +5,48 @@
     .offcanvas {
         width: 300px;
     }
-    @media (max-width: 800px) {
+    @media (max-width: 1000px) {
        .offcanvas {
         max-width: 60%;
        }
+       #navbarMenuInline {
+        display: none;
+       }
     }
+    @media (min-width: 1000px) {
+        #navbarMenuOffcanvas {
+            display: none;
+        }
+        #navbarMenuInline {
+            display: flex;
+            flex-direction: row;
+            align-content: center;
+            justify-content: space-between;
+        }
+        li {
+            padding: 0px 20px;
+            text-decoration: none;
+        }
+        ul {
+            padding: 0 0 0 0;
+            margin: 0 0 0 0;
+            list-style: none;
+        }
+        .navbar-link {
+            text-decoration: none;
+            color: white;
+        }
+        .list-item {
+            color: white;
+        }
+    }
+    #signInButton {
+        text-decoration: none;
+        color: white;
+    }
+    #signInButton:hover {
+        text-decoration: underline;
+    }   
 </style>
 
 <script>
@@ -18,7 +55,36 @@ export default {
     methods: {
         logOut() {
             window.sessionStorage.clear();
+            this.isLoggedIn();
+        },
+        isLoggedIn() {
+            if (window.sessionStorage.getItem('userID')) {
+                this.isLogged = true
+            } else {
+                this.isLogged = false
+            }
         }
+    },
+    data() {
+        return {
+            userDetails: {
+                firstName: window.sessionStorage.getItem('firstName'),
+                lastName: window.sessionStorage.getItem('lastName'),
+                userID: window.sessionStorage.getItem('userID')
+            },
+            isLogged: window.sessionStorage.getItem('isLoggedIn')
+        }
+    },
+    computed: {
+        userSalutation() {
+            return 'Hi, ' + this.userDetails.firstName;
+        }
+    },
+    mounted() {
+        this.isLoggedIn();
+    },
+    updated() {
+        this.isLoggedIn();
     }
 }
 
@@ -32,58 +98,112 @@ export default {
                 to="/">
                 Groupomania
             </router-link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Groupomania - Menu</h5>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item"
-                            data-bs-dismiss="offcanvas">
-                            <router-link 
-                                class="nav-link active" 
-                                aria-current="page"
-                                to="/"
-                                >
-                                Home
-                            </router-link>
-                        </li>
 
-                        <li class="nav-item"
-                            data-bs-dismiss="offcanvas">
+            <div class="navbar-menu-inline">
+                <ul id="navbarMenuInline">
+                    <li class="list-item"
+                        v-if="!isLogged"
+                        > Hi, 
                             <router-link 
-                                class="nav-link"
+                                id="signInButton"
                                 to="/loginpage">
-                                Login
+                                sign in
                             </router-link>
-                        </li>
+                    </li>
+                    <li class="list-item"
+                        v-if="isLogged"
+                        >
+                        {{ userSalutation }}
+                    </li>
 
-                        <li class="nav-item"
-                            data-bs-dismiss="offcanvas">
+                    <li>
+                        <router-link 
+                            class="navbar-link" 
+                            to="/">
+                            Home
+                        </router-link>
+                    </li>
+
+                    <li>
+                        <router-link 
+                            class="navbar-link"
+                            to="/createpost">
+                            Create Post
+                        </router-link>
+                    </li>
+
+                    <li>
+                        <router-link 
+                            class="navbar-link"
+                            to="/"
+                            v-if="isLogged"
+                            @click="logOut">
+                            Logout
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+
+            <div id="navbarMenuOffcanvas">
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" 
+                            id="offcanvasDarkNavbarLabel"
+                            v-if="!isLogged"
+                            data-bs-dismiss="offcanvas"> Hi, 
                             <router-link 
-                                class="nav-link"
-                                to="/createpost">
-                                Create Post
+                                id="signInButton"
+                                to="/loginpage">
+                                sign in
                             </router-link>
-                        </li>
+                        </h5>
+                        <h5 class="offcanvas-title" 
+                            id="offcanvasDarkNavbarLabel"
+                            v-if="isLogged">
+                            {{ userSalutation }}
+                        </h5>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item"
+                                data-bs-dismiss="offcanvas">
+                                <router-link 
+                                    class="nav-link active" 
+                                    aria-current="page"
+                                    to="/"
+                                    >
+                                    Home
+                                </router-link>
+                            </li>
 
-                        <li><hr style="width: 100%; height: 2px"></li>
+                            <li class="nav-item"
+                                data-bs-dismiss="offcanvas">
+                                <router-link 
+                                    class="nav-link"
+                                    to="/createpost">
+                                    Create Post
+                                </router-link>
+                            </li>
 
-                        <li class="nav-item"
-                            data-bs-dismiss="offcanvas">
-                            <router-link 
-                                class="nav-link"
-                                to="/"
-                                @click="logOut">
-                                Logout
-                            </router-link>
-                        </li>
+                            <li><hr style="width: 100%; height: 2px"></li>
 
-                    </ul>
+                            <li class="nav-item"
+                                data-bs-dismiss="offcanvas">
+                                <router-link 
+                                    class="nav-link"
+                                    to="/"
+                                    @click="logOut">
+                                    Logout
+                                </router-link>
+                            </li>
+
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
