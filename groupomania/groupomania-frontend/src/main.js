63f00604c40851299054c12f7195ce7d7f0d1b2e
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-13 02:09:33 +0000

    pinia and fontawesome imported

--- groupomania/groupomania-frontend/src/main.js
+++ groupomania/groupomania-frontend/src/main.js
@@ -1,16 +1,27 @@
 import "bootstrap/dist/css/bootstrap.css";
 
+import { createPinia } from 'pinia';
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp as farThumpsUp  } from '@fortawesome/free-regular-svg-icons';
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+
+library.add(faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, farThumpsUp, faRightFromBracket);
+
+const pinia = createPinia();
 
 const app = createApp(App);
+app.use(pinia);
 app.use(router);
 app.use(VueAxios, axios);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount("#app");
-
 
 import "bootstrap/dist/js/bootstrap.js";
