f8f3be6547c15bc156b76d6b603136472a1b0aea
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-13 02:10:43 +0000

    dropdown and icons implemented

--- groupomania/groupomania-frontend/src/components/PostCard.vue
+++ groupomania/groupomania-frontend/src/components/PostCard.vue
@@ -1,4 +1,4 @@
-<style>
+<style scoped>
   body {
     background-color: #eee
   }
@@ -19,10 +19,30 @@
     height: auto
   }
 
+  .font-size-custom {
+    font-size: 13px;
+    display: flex;
+    align-items: flex-end;
+  }
+  .card-link:hover .card-container {
+    border-color: blue;
+  }
+
+  .card-container {
+    border-color: blue;
+  }
+ 
+
 </style>
 
 <script>
+  import { usePostStore } from '../stores/PostStore';
+
   export default {
+    setup() {
+      const postStore = usePostStore();
+      return { postStore }
+    },
     name: 'PostCard',
     props: {
       post: {
@@ -32,12 +52,13 @@
     },
     data() {
       return {
-        isOwner: false
+        isOwner: false,
+        isLiked: true
       }
     },
     methods: {
       userCheck() {
-        if (this.post.userID == sessionStorage.getItem('userID')) {
+        if (this.post.userID == localStorage.getItem('userID')) {
           this.isOwner = true;
         }
       }
@@ -80,49 +101,91 @@
 </script>
 
 <template>
-   
-  <div class="bg-white border mt-2">
+  <router-link to="" class="card-link text-decoration-none text-reset">
+    <div class="card-container bg-white border mt-4 shadow">
 
-    <div>
-      <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
+      <div>
+        <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
 
-        <div class="d-flex flex-row align-items-center feed-text px-2">
-          <img 
-            class="rounded-circle" 
-            src="https://i.imgur.com/aoKusnD.jpg" 
-            width="45">
-          
-          <div class="d-flex flex-column flex-wrap ml-2">
-            <span class="font-weight-bold px-2"> {{ post.firstName + ' ' + post.lastName }}</span>
-            <span class="text-black-50 time px-2"
-                  > {{ postTenure }}</span>
+          <div class="d-flex flex-row align-items-center feed-text px-2">
+            <img 
+              class="rounded-circle" 
+              src="https://i.imgur.com/aoKusnD.jpg" 
+              width="45">
+            
+            <div class="d-flex flex-row flex-wrap ml-2">
+              <span class="font-weight-bold px-2 text-capitalize">{{ '/' + post.postCategory }}</span>
+              <span class="font-weight-bold">.</span>
+              <span class="font-size-custom text-secondary px-2"> 
+                {{ 'Posted by ' + post.firstName + ' ' + post.lastName + ' ' + postTenure + ' ago'  }}
+              </span>
+            </div>
+          </div>
+
+          <div>
+            
+          </div>
+
+          <div v-if="this.isOwner" class="px-3">
+            <a
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false">
+              <font-awesome-icon icon="fa-solid fa-ellipsis-vertical"/>
+            </a>
+            <ul
+              class="dropdown-menu"
+              aria-labelledby="dropdownMenuButton1">
+              <li>
+                <a
+                
+                class="dropdown-item"
+                href="#">Edit</a>
+              </li>
+              <li>
+                <a
+                class="dropdown-item"
+                href="#">
+                Delete</a>
+              </li>
+            </ul>
           </div>
         </div>
-
-        <div>
-          <span class="font-weight-bold px-2 text-capitalize">{{ post.postCategory }}</span>
-        </div>
-
-        <!-- <div class="feed-icon px-2">
-          <i class="fa fa-ellipsis-v text-black-50"></i>
-        </div> -->
-
       </div>
+
+      <div class="p-2 px-3">
+          <span>
+
+            {{post.postText}}
+
+          </span>
+      </div>
+
+      <div class="d-flex justify-content-end socials">
+          <span class="py-2">
+            <a href="" :class="{ 'text-danger': this.isLiked }">
+              <font-awesome-icon icon="fa-solid fa-thumbs-up"/>
+            </a>
+            
+          </span>
+          <div class="py-2 px-3">
+            <span> {{ postStore.likesCount }} likes</span>
+          </div>
+
+          <span class="py-2">
+            <a href="" class="text-reset">
+              <font-awesome-icon icon="fa-solid fa-comment"/>
+            </a>
+          </span>
+          <div class="py-2 px-3">
+            <span> {{ postStore.commentsCount }} comments</span>
+          </div>
+          
+      </div>
+
     </div>
-
-    <div class="p-2 px-3">
-        <span>
-
-          {{post.postText}}
-
-        </span>
-    </div>
-
-    <div class="d-flex justify-content-end socials p-2 py-3">
-        <i class="fa-solid fa-thumbs-up"></i>
-        <i class="fa-solid fa-comment"></i>
-    </div>
-
-  </div>
+  </router-link> 
+ 
 
 </template>
