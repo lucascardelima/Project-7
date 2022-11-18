b9fdbe9c9fb91314fab872a7313e3c48d6348665
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-18 23:40:09 +0000

    cleaned the script

--- groupomania/groupomania-frontend/src/views/HomePage.vue
+++ groupomania/groupomania-frontend/src/views/HomePage.vue
@@ -5,11 +5,15 @@
 <script >
   import axios from 'axios';
   import PostCard from '../components/PostCard.vue'
+  
   axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
   axios.defaults.headers.post['Authorization'] = `Bearer ${localStorage.getItem('token')}`
     
   export default {
     name: 'HomePage',
+    components: {
+      PostCard
+    },
     data() {
       return {
         requestDetails: {
@@ -20,50 +24,53 @@
         posts: []
       }
     },
-    mounted() {
-      axios.post('http://localhost:3000/api/posts/getposts', this.requestDetails).then(
+    methods: {
+      getPosts() {
+        axios.post('http://localhost:3000/api/posts/getposts', this.requestDetails).then(
         (response) => {
           this.addPosts(response.data);
         }
-    ).catch(
-      (error) => {
-        console.log(error)
-      }
-    )
-    },
-    components: {
-      PostCard
-    },
-    methods: {
-      getPosts() {
-        axios.get('http://localhost:3000/api/posts/getposts', this.requestDetails, {
-          headers: {
-            'Authorization': 'Bearer ' + this.token 
-          }
-        })
-      },
+      ).catch(
+        (error) => {
+          console.log(error)
+        }
+      )},
       addPosts(posts) {
         this.posts.push(posts)
-      }
+      },
+      deletePost(deletedPostID) {
+        let i = 0;
+
+        for (i = 0; i < this.posts[0].length; i++) {
+
+          if (this.posts[0][i].postID == deletedPostID) {
+            this.posts[0].splice(i, 1);
+          }
+        }
+      }      
+    },
+    mounted() {
+      this.getPosts();
     }
   }
 </script>>
 
 <template>
 
-<div class="container mt-4 mb-5">
-  <div class="d-flex justify-content-center row">
-    <div class="col-md-8">
-      <div class="feed p-2">
-           
-        <PostCard v-for="post in posts[0]"
-                  :post="post"
-                  :key="post.postID"/>
+  <div class="container mt-4 mb-5">
+    <div class="d-flex justify-content-center row">
+      <div class="col-md-8">
+        <div class="feed p-2">
+                    
+          <PostCard v-for="post in posts[0]"
+                    :post="post"
+                    :key="post.postID"
+                    @delete-post="deletePost"/>
 
+        </div>
       </div>
     </div>
   </div>
-</div>
 
     
 </template>
