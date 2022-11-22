bc95384dac9667de0cc7eb1284da6eb528ad3711
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-22 21:48:07 +0000

    add trash can as icon to comment card

--- groupomania/groupomania-frontend/src/main.js
+++ groupomania/groupomania-frontend/src/main.js
@@ -7,13 +7,13 @@ import router from "./router";
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, faRightFromBracket, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { faThumbsUp as farThumpsUp  } from '@fortawesome/free-regular-svg-icons';
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 
-library.add(faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, farThumpsUp, faRightFromBracket);
+library.add(faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, farThumpsUp, faRightFromBracket, faTrashCan);
 
 const pinia = createPinia();
 
