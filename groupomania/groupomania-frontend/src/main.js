d0075f12e2f63d714fc06d9bce2e01ef581c0c6e
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-23 01:19:13 +0000

    added user icon

--- groupomania/groupomania-frontend/src/main.js
+++ groupomania/groupomania-frontend/src/main.js
@@ -7,13 +7,13 @@ import router from "./router";
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, faRightFromBracket, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, faRightFromBracket, faTrashCan, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faThumbsUp as farThumpsUp  } from '@fortawesome/free-regular-svg-icons';
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 
-library.add(faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, farThumpsUp, faRightFromBracket, faTrashCan);
+library.add(faThumbsUp, faComment, faEllipsisVertical, faPlus, faHouse, farThumpsUp, faRightFromBracket, faTrashCan, faUser);
 
 const pinia = createPinia();
 
