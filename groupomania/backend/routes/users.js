ee38b5fd907f3889afce4e00a72b6176942b73e2
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-11-28 04:01:47 +0000

    delete user router

--- groupomania/backend/routes/users.js
+++ groupomania/backend/routes/users.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const usersCtrl = require('../controllers/users');
 
-router.get('/getusers', usersCtrl.getUsers);
 router.get('/getuser', usersCtrl.getUser);
 router.post('/login', usersCtrl.login);
 router.post('/signup', usersCtrl.signup);
 router.post('/updateuser', usersCtrl.updateUser);
+router.post('/deleteuser', usersCtrl.deleteUser)
 
 module.exports = router;
