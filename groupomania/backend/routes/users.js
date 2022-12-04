777b9efec7229dfe5db20581120a9ca69fa847d7
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-12-04 22:45:47 +0000

    added the auth middleware to the delete user route

--- groupomania/backend/routes/users.js
+++ groupomania/backend/routes/users.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const usersCtrl = require('../controllers/users');
+const auth = require('../middleware/auth');
 
 router.get('/getuser', usersCtrl.getUser);
 router.post('/login', usersCtrl.login);
 router.post('/signup', usersCtrl.signup);
 router.post('/updateuser', usersCtrl.updateUser);
-router.post('/deleteuser', usersCtrl.deleteUser)
+router.post('/deleteuser', auth, usersCtrl.deleteUser);
 
 module.exports = router;
