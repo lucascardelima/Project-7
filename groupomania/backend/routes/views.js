9cc7de354c14450035522d5bd27813980aa04a22
Lucas Marchetti de Lima <80517845+lucascardelima@users.noreply.github.com>
2022-12-04 22:46:12 +0000

    added auth mid to the views route

--- groupomania/backend/routes/views.js
+++ groupomania/backend/routes/views.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const viewsCtrl = require('../controllers/views');
-// const auth = require('../middleware/auth')
+const auth = require('../middleware/auth')
 
-router.post('/', viewsCtrl.viewPost);
-router.post('/getviews', viewsCtrl.getViews);
+router.post('/', auth, viewsCtrl.viewPost);
+router.post('/getviews', auth, viewsCtrl.getViews);
 
 
 module.exports = router;
